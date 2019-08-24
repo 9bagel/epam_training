9e49df67c207d080a590468c9353b7caa3eb9236
Dzianis Bahlei <9bagel@gmail.com>
2019-08-25 01:16:20 +0300

    commit

---
+++ src/com/epam/basic_of_oop/dragon_cave/treasure/TreasureName.java
@@ -0,0 +1,23 @@
+package com.epam.basic_of_oop.dragon_cave.treasure;
+
+import java.util.Random;
+
+public enum TreasureName {
+    KINGS_CROWN,
+    GOLDEN_CUP,
+    GOLDEN,
+    SILVER_TABLEWARE,
+    ROYAL_SKYPETER,
+    BELT_DECORATED_WITH_GOLD,
+    FUR_GOWN,
+    LEAKY_SHIRT,
+    TREASURE_CHEST;
+
+    private static final TreasureName[] TREASURE_NAMES = values();
+    private static final int SIZE = TreasureName.values().length;
+    private static final Random RANDOM = new Random();
+
+    public static TreasureName getRandomName() {
+        return TREASURE_NAMES[RANDOM.nextInt(SIZE)];
+    }
+}
